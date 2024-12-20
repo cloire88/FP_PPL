f43e1eeb2bbcd7b97cf976bb60b84af5a67c4dfe
cloire88 <141556252+cloire88@users.noreply.github.com>
2024-12-20 17:01:20 +0700

    Add files via upload

---
+++ FPPPL/FPPPL/IDbServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FPPPL
+{
+    public interface IDbServices
+    {
+        public void AddToDatabase()
+        {
+            var connection = new DatabaseConnection("jdbc:mysql://localhost:3306/mydatabase", "admin", "rahasia");
+            connection.Connect();
+        }
+    }
+}
